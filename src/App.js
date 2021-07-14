ca3ddc4e86a3b094aae34824bbc22686f043dff0
ViktoriaMik <victoriamik2015@gmail.com>
2021-07-14 17:02:46 +0300

    17:02

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+
 import './App.css';
 import AllMovies from "./components/AllMovies";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
 import Navigation from "./navigation/navigation";
-import SimpleSlider from "./components/sliderComponent";
 
 
 function App() {
+
     return (
+
         <Provider store={store}>
 
             <Navigation/>
         </Provider>
+
 
     );
 }
